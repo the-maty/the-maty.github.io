4cc11414889f7275bb13e13400f1e250c8c0c8f2
Matyáš Havlík <52372122+the-maty@users.noreply.github.com>
2023-01-28 17:20:08 +0100

    Hamburger menu removed
    , no need for this

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-const hamburger = document.querySelector('.hamburger');
-const navBar = document.querySelector('.navbar');
+document.querySelector("#kontakt-form").addEventListener("submit", function(event){
+// vypneme výchozí chování formuláře
+event.preventDefault()
+// načtení hodnot z políček
+let Name = event.target.elements.Name.value
+let email = event.target.elements.email.value
+let Tel = event.target.elements.Tel.value
+// vytvoříme odstavec a přidáme do něj text z políčka
+let paragraph = document.createElement("p")
 
-hamburger.addEventListener('click', () => {
-navBar.classList.toggle('burger-hide-btn');       
-});
-
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('navbar a').forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active-link');
-    console.log(link);
-  }
+paragraph.innerHTML = `Jméno: ${Name}, <br>
+Email: ${email}, <br> Telefon: ${Tel}`
+document.querySelector("#from-form").appendChild(paragraph)
+// po odeslání vymažeme obsah políček
+event.target.elements.Name.value = ""
+event.target.elements.email.value = ""
+event.target.elements.Tel.value = ""
 })
