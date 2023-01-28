bd7101dd7f3dfaf12a79009fe7ef6bdf562a6136
Matyáš Havlík <52372122+the-maty@users.noreply.github.com>
2023-01-28 17:21:39 +0100

    Javascript added
    For contact form script to print out value on site for now

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 document.querySelector("#kontakt-form").addEventListener("submit", function(event){
 // vypneme výchozí chování formuláře
 event.preventDefault()
+    
 // načtení hodnot z políček
 let Name = event.target.elements.Name.value
 let email = event.target.elements.email.value
 let Tel = event.target.elements.Tel.value
+
 // vytvoříme odstavec a přidáme do něj text z políčka
 let paragraph = document.createElement("p")
-
 paragraph.innerHTML = `Jméno: ${Name}, <br>
 Email: ${email}, <br> Telefon: ${Tel}`
 document.querySelector("#from-form").appendChild(paragraph)
+    
 // po odeslání vymažeme obsah políček
 event.target.elements.Name.value = ""
 event.target.elements.email.value = ""
